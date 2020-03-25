6d465a6681e9bd6278aed8ad31de558a1df9d3f7
nobu.azu <jonsonjonson1210@gmail.com>
2020-03-25 12:19:59 +0000

    add favorites likes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,7 @@ class Micropost < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 255 }
 
+  has_many :favorites
+  has_many :favorites_user, through: :favorites, source: :user
+
 end
